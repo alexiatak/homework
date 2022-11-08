010de2fdeb9719879362944df50d9e7ac3ded4fd
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-11-09 00:13:03 +0200

    Update ex114.py

--- ex114.py
+++ ex114.py
@@ -1,18 +1,13 @@
 #ex 1.14 "LISTS"
 lst = list(map(int, input("Enter the integer/elements:").strip().split()))
-lst.sort()
 print(lst)
+def list_split(lst):
+    res = []
+    for i in lst:
+        lstsmall = [i for k in range(lst.count(i))]
+        if lstsmall not in res:
+            res.append(lstsmall)
+    return res
 
-lstbig=[]
-for i in list(lst):
-    if lst(i).attr !=  lst(i-1).attr :
-        print(lst.count(i))   #how many times a number is repeated
-        lstsmall=[] 
-        for j in range(lst.count(i)):
-            lstsmall.append(i)      #we add the multiplied numbers to a smaller list
-            print(lstsmall) 
-       #if lstsmall(j) not in lstbig(i):
-        lstbig.append(lstsmall)       #we add all the small lists to a big list
-   
-print(lstbig)
-#res=set(lstbig)
+
+print(list_split(lst))
