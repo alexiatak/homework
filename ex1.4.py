eb0923b0911b51ec102737d724fc50ded5532677
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-11-09 12:10:56 +0300

    Update ex1.4.py

--- ex1.4.py
+++ ex1.4.py
@@ -1,13 +1,13 @@
 #ex1.4 CIPHER
 def encrypt(text,s):
-    result = []
+    result = ""
     for i in range(len(text)):
       char = text[i]   
       if (char.isupper()):
          result += chr((ord(char) + s-65) % 26 + 65)
       else:
         result += chr((ord(char) + s - 97) % 26 + 97)
-      return result
+    return result
 text = "hey you"
 s = 2
 
