087189918eb4fbb56481b40c0a7c8c2a4a626b1e
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-11-15 15:15:45 +0300

    Update ex1.4.py

--- ex1.4.py
+++ ex1.4.py
@@ -14,3 +14,18 @@ s = 2
 print ("Plain Text : " + text)
 print ("Shift : " + str(s))
 print (encrypt(text,s))
+def decrypt(text,s):
+    result = ""
+    for i in range(len(text)):
+      char = text[i]   
+      if (char.isupper()):
+         result += chr((ord(char) - s-65) % 26 + 65)
+      else:
+        result += chr((ord(char) - s - 97) % 26 + 97)
+    return result
+text = "jgapaqw"
+s = 2
+
+print ("Encrypted Text : " + text)
+print ("Shift : " + str(s))
+print (decrypt(text,s))
