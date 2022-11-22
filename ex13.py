07aa167df96cd04d6e66ac00ffdab16676725420
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-11-23 00:52:01 +0300

    Update ex13.py

--- ex13.py
+++ ex13.py
@@ -1,16 +1,14 @@
 #ex1.3 NEARLY SORTED
 lst = list(map(int, input("Enter the integer/elements:").strip().split()))
 print(lst)
-count=0
-def nearly_sorted(lst):
-	res = []
-	for i in lst:
-		if lst[i] < lst[i-1] : 
-			del lst[i]
-			count+=1
-	return result
-if count==1 :
-	print("TRUE")
+
+def nearly_sorted(lst,ccount):
+	ccount=0
+	for i in (range(len(lst)-1)):
+		if lst[i+1] < lst[i] :
+			ccount+=1
+	return ccount
+if ccount==1 :
+	print("TRUE")		
 else:
 	print("FALSE")
-print(nearly_sorted)
