799441aca61758c3651263cc2836865aff23eaa0
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-11-22 21:49:49 +0300

    Update ex25.py

--- ex25.py
+++ ex25.py
@@ -1,16 +1,23 @@
-class Star(object):
-	def __init__(self,absmag):
+class Star:
+	def __init__(self, absmag):
 		self.absmag = absmag
+	def __repr__(self):
+		return f"{self.absmag}"
 		
-sun = Star(absmag=1)
-vega = Star(absmag=0)
-
-liist=[]
-liist.append(sun)
-liist.append(vega)
+def find_cluster_magnitude(stars,pc):
+	import math
+	for star in stars:
+		s = 0
+		s = s + 10**(-0.4*(star.absmag))
+		sabsolute = -2.5*(math.log10(s))
+		sapparent= sabsolute + 5 - 5*(math.log10(pc))
+	return sapparent
+sun=Star(4.8)
+vegas=Star(0.58)
+sirius=Star(1.4)
+stars=()
+stars=(sun,vegas,sirius)
+res=find_cluster_magnitude(stars,4)
+print(res)
+liist=sorted(stars,key=star.absmag)
 print(liist)
-with open('stars.dat', "w") as s:
-	for item in liist:
-		s.write("%s\n" % item)
-	print('Done')
-		
