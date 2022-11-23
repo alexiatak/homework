054280445d089d70599bc82f4ca475dc44014f72
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-11-23 10:45:50 +0300

    Create ex26.py

---
+++ ex26.py
@@ -0,0 +1,19 @@
+class FunctionCaller:
+	def __init__(self,fun):
+		self.fun=fun
+		self.liist = []
+	@classmethod
+	def append_job(cls,exercise):
+		cls.liist.append(exercise)
+		#self.append_job=fun.append(exercise)
+		#return cls(exercise)
+		
+	@classmethod
+	def call(cls):
+		for i in range(len(cls.liist)):
+			print (f"Calling for job {exercise} . RESULT {cls(exercise)}")
+
+from math import sin
+caller=FunctionCaller(sin)
+caller.append_job(0)
+caller.call()
