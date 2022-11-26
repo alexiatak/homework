cef852bbf15524c03632f54ab07fb8e16973979c
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-11-26 16:12:52 +0300

    updated26.py

--- ex26.py
+++ ex26.py
@@ -2,16 +2,24 @@ class FunctionCaller:
 	def __init__(self,fun):
 		self.fun=fun
 		self.liist = []
-	@classmethod
-	def append_job(cls,exercise):
-		cls.liist.append(exercise)
+	
+	def append_job(self,exercise): 
+		self.liist.append(exercise)
 		
-	@classmethod
-	def call(cls):
-		for i in range(len(cls.liist)):
-			print (f"Calling for job {exercise} . RESULT {cls(exercise)}")
-
+	res=[]
+	def call(self):
+		if len(self.liist) != 0 :
+			for i in range(len(self.liist)):
+				res(i) == self.fun(self.liist(i))
+				print (f"Calling for job {self.liist(i)} . RESULT" ,res(i))
+			self.liist.clear()
+		else:
+			print("nothing to do")
+			
 from math import sin
 caller=FunctionCaller(sin)
 caller.append_job(0)
+caller.append_job(1)
+caller.append_job(3.1415)
+caller.call()
 caller.call()
