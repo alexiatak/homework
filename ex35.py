20d09ee23f988a7c40fe447e5e91b898744599df
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-12-06 22:46:17 +0300

    Create ex35.py

---
+++ ex35.py
@@ -0,0 +1,23 @@
+#changes in directory
+import os
+import datetime
+import time
+  
+myadress=os.getcwd()
+
+def scanner(adress):
+	while(True):
+		dir_list = os.listdir(adress)
+		dir_before=set(dir_list)
+		time.sleep(1)
+		dir_list = os.listdir(adress)	
+		dir_after=set(dir_list)
+		timee=datetime.datetime.now()
+		
+		if dir_before != dir_after :
+			if dir_before.difference(dir_after) != set(dir_list):
+				print (f"in { timee } , {dir_before.difference(dir_after)} was removed")
+			elif dir_after.difference(dir_before) != set(dir_list):
+				print (f"in { timee } , {dir_after.difference(dir_before)} was created")
+
+scanner(myadress)
