d70b6319678a52d3549f6cb004bd8412e144b9ec
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-12-07 10:44:22 +0300

    Update ex33.py

--- ex33.py
+++ ex33.py
@@ -5,8 +5,8 @@ import os
 myadress=os.getcwd()
 read_files = glob.glob("*.txt")
 
-with open("resultof33.txt", "wb") as outfile:
+with open("resultof33.txt", "w") as outfile:
     for f in read_files:
-        with open(f, "rb") as infile:
-            outfile.write(f"from {f} : ")
-            outfile.write(infile.read())
+       with open(f, "r") as infile:
+            outfile.write(f"from {f} : \n")
+            outfile.write(infile.read() + "\n")
