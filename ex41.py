aacbd0cb33cedff195deaf0a33dca4d464b454b4
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-12-20 20:58:43 +0300

    Update ex41.py

--- ex41.py
+++ ex41.py
@@ -1,28 +1,35 @@
-#cores in this pc
 from multiprocessing import Process as pr
 import time
+import math
 
 def examplee(x):
-	sqr=x*x
-	return sqr
+	res = 0
+	for _ in range(math.ceil(x)):
+		res += 1
+	return res
 
 
 if __name__=='__main__':
 	runtime=[]
 	all_pr=[]
-	x=1000000000
+	x=10000000
 
-	while True:
-		for i in range(48):
-			example_pr=pr(target=examplee, args=(x, ))
-			all_pr.append(example_pr)
-			for example_pr in all_pr:
-				example_pr.start()
-			starting=time.time()
-			for example_pr in all_pr:
-				example_pr.join()
-			finishing=time.time()
-			runtime.append(finishing-starting)
-			if runtime[i]>runtime[i-1]:
-				break
+	for i in range(1, 48):
+		all_pr = []
+		print(f"i = {i}")
+		for j in range(1, i + 1):
+			all_pr.append(pr(target=examplee, args=(x / i, )))
+		print(all_pr)
+		for example_pr in all_pr:
+			example_pr.start()
+		starting=time.time()
+		print(starting)
+		for example_pr in all_pr:
+			example_pr.join()
+		finishing=time.time()
+		print(finishing)
+		runtime.append(finishing-starting)
+		print(runtime[-1])
+		if i > 1 and runtime[i-1]>runtime[i-2]:
+			break
 	print(f"Cores in this pc:{i}")
