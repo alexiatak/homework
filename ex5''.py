3eb236d3d4843d8f3e7609870869f1bda7d957f7
alexiatak <113921367+alexiatak@users.noreply.github.com>
2022-12-27 20:14:53 +0300

    Update ex5''.py

--- ex5''.py
+++ ex5''.py
@@ -23,20 +23,20 @@ def stand_deviation(lst):
 
 j=10
 
-def one_dim_list(j)
-	Alist=MyList([MyList([randint(10,100)]*j)])
-	Blist=MyList([MyList([randint(10,100)]*j)])
-	return one_dim
+def one_dim_list(j):
+	Alist=MyList([MyList([random.randint(10,100)]*j)])
+	Blist=MyList([MyList([random.randint(10,100)]*j)])
+	return Alist,Blist
 
-def two_dim_list(j)
-	Alist=MyList([MyList([randint(10,100)]*j) for col in range(j)])
-	Blist=MyList([MyList([randint(10,100)]*j) for col in range(j)])
-	return two_dim
+def two_dim_list(j):
+	Alist=MyList([MyList([random.randint(10,100)]*j) for col in range(j)])
+	Blist=MyList([MyList([random.randint(10,100)]*j) for col in range(j)])
+	return Alist,Blist
 
-def three_dim_list(j)
-	Alist=MyList([MyList([MyList([randint(10,100)]*j)for col in range(j)]) for row in range(j)])
-	Blist=MyList([MyList([MyList([randint(10,100)]*j)for col in range(j)]) for row in range(j)])
-	return three_dim
+def three_dim_list(j):
+	Alist=MyList([MyList([MyList([random.randint(10,100)]*j)for col in range(j)]) for row in range(j)])
+	Blist=MyList([MyList([MyList([random.randint(10,100)]*j)for col in range(j)]) for row in range(j)])
+	return Alist,Blist
 
 colours = ["#FF7F50", "#00FFFF", "#FFD700"]
 sample_size=20
@@ -44,6 +44,8 @@ averaget=[]
 runtime=[]
 sigma=[]
 sizee=10
+Alist=[]
+Blist=[]
 
 #test for one dim list
 one_dim_list(j)
@@ -86,3 +88,35 @@ for i in range(sample_size):
 plt.errorbar(n ,averaget, yerr=sigma, fmt="o", ecolor=colours[2], capsize=3, color=colours[2], label=f"{3}-dimension list")
 plt.legend(fontsize=12)
 plt.plot(sizee, averaget, color=colours[2])
+
+
+#creating np one dim array
+def one_dim_array
+	Aarray=np.random.randint(low=10, high=100, size=(j, 1))
+	Barray=np.random.randint(low=10, high=100, size=(j, 1))
+	return Aarray , Barray 
+
+def two_dim_array
+	Aarray=np.random.randint(low=10, high=100, size=(j, 2))
+	Barray=np.random.randint(low=10, high=100, size=(j, 2))
+	return Aarray , Barray
+
+def three_dim_array
+	Aarray=np.random.randint(low=10, high=100, size=(j, 3))
+	Barray=np.random.randint(low=10, high=100, size=(j, 3))
+	return Aarray , Barray
+
+
+npruntime1=[]
+npaveraget1=[]
+npsigma1=[]
+
+#multiplication of 1 dim np arrays
+for i in range(len(Aarray)):
+	npstarting=time()
+		for _ in range(j):
+			np.multiply(Aarray,Barray)
+	npfinishing=time()
+	npruntime1.append(npfinishing-npstarting)
+	npaveraget1.append(average(npruntime1))
+	npsigma1.append(stand_deviation(npruntime1))
